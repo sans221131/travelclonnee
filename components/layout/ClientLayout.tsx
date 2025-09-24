fb5a9db823c1846bfd353edb00705178ce8e8c2f
sans221131 <somaanransari@gmail.com>
2025-09-25 03:33:49 +0530

    Update ClientLayout component

--- components/layout/ClientLayout.tsx
+++ components/layout/ClientLayout.tsx
@@ -3,13 +3,23 @@
 import { CartProvider } from "@/contexts/CartContext";
 import CartDrawer from "@/components/cart/CartDrawer";
 import CartNotification from "@/components/cart/CartNotification";
+import { usePathname } from "next/navigation";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  
+  // Show cart only on activities and trip receipt pages
+  const showCart = pathname.includes('/activities') || pathname.includes('/trip/receipt');
+
   return (
     <CartProvider>
       {children}
-      <CartDrawer />
-      <CartNotification />
+      {showCart && (
+        <>
+          <CartDrawer />
+          <CartNotification />
+        </>
+      )}
     </CartProvider>
   );
 }
